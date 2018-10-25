d8bf3067f62eaa701c1b6741de633bfa405bea3c
Madhav Bahl <madhavbahl10@gmail.com>
2018-10-26 00:00:29 +0530

    Connect C script with node

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const hbs = require('hbs');
+const fs = require('fs');
 
 const port = process.env.PORT || 8000;
 
@@ -9,13 +10,35 @@ app.use(bodyParser());
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'hbs');
+var exec = require("child_process").exec;
 
 app.get('/', (req, res) => {
     res.render('index.hbs');
 });
 
 app.get('/tsp', (req, res) => {
-    res.send('<h1>Let\'s solve TSP');
+    res.render('tsp.hbs');
+});
+
+app.post('/solution', (req, res) => { 
+    finalinput = req.body.numNodes + '\r\n' + req.body.matrix;
+    fs.writeFile('input.in', finalinput, (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+        exec("make < input.in", (err, stdout) => {
+            if (err) {
+                throw err;
+            }
+    
+            console.log(stdout);
+            stdout = stdout.split('\n');
+            res.send({
+                mincost: stdout[2],
+                path: stdout[3]
+            });
+        });
+    });
+    
 });
 
 app.listen (port, () => {
--- tsp_openmp.c
+++ tsp_openmp.c
@@ -132,6 +132,6 @@ int main()
     printf("Path Taken : ");
     for (i=0; i<=N; i++) 
         printf("%d ", final_path[i]);
-    // printf ("\n\n==================================\n\n");
+    printf ("\n\n==================================\n\n");
     return 0;
 }
